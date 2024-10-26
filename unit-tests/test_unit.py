048e1a79c5dfc52c5eb355b0021a30e16cde8662
Priyanshu Raj <50186075+Priyanshu-Raj@users.noreply.github.com>
2024-10-26 23:17:05 +0530

    Update test_unit.py

--- unit-tests/test_unit.py
+++ unit-tests/test_unit.py
@@ -1,6 +1,12 @@
 import pytest
 from fastapi.testclient import TestClient
 from apps.OnlineCalculator import app  
+import sys
+import os
+
+# Adjust the path to include the root project directory
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
 
 client = TestClient(app)
 
